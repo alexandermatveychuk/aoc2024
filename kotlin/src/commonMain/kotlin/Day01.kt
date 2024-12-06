bce78e87834b64af609cc2ecda0d515c5eeddc13
Alexander Matveychuk <alexandermatveychuk@users.noreply.github.com>
2024-12-06 14:48:57 +0200

    day 1 part 2 on kotlin

--- kotlin/src/commonMain/kotlin/Day01.kt
+++ kotlin/src/commonMain/kotlin/Day01.kt
@@ -1,16 +1,12 @@
+import kotlin.collections.component1
+import kotlin.collections.component2
 import kotlin.math.abs
 
 object Day01 {
     fun part1(input: List<String>): Int {
         return input
             .asSequence()
-            .map { line ->
-                line.split(' ')
-                    .mapNotNull { it.toIntOrNull() }
-                    .takeIf { it.size == 2 }
-                    ?.let { (first, second) -> first to second }
-                    ?: throw RuntimeException("Line couldn't be converted")
-            }
+            .map { it.toPairOfNumbers() }
             .unzip()
             .let { (first, second) ->
                 first.sorted()
@@ -19,8 +15,34 @@ object Day01 {
             }
     }
 
+    fun part2(input: List<String>): Int {
+        return input
+            .asSequence()
+            .map { it.toPairOfNumbers() }
+            .unzip()
+            .let { (first, second) ->
+                val secondMap = mutableMapOf<Int, Int>()
+                for (i in second) {
+                    val c = secondMap[i] ?: 0
+                    secondMap[i] = c + 1
+                }
+                first.fold(0) { acc, i ->
+                    acc + (secondMap[i] ?: 0) * i
+                }
+            }
+    }
+
     fun main() {
         val input = readInput("input01")
         part1(input).println()
+        part2(input).println()
     }
+}
+
+private fun String.toPairOfNumbers(): Pair<Int, Int> {
+    return split(' ')
+        .mapNotNull { it.toIntOrNull() }
+        .takeIf { it.size == 2 }
+        ?.let { (first, second) -> first to second }
+        ?: throw RuntimeException("Line couldn't be converted")
 }
